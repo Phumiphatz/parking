285540db71efa7e0808948b179c08faa8c590e67
Phumiphat Wongchuea <demopumiphat@gmail.com>
2019-08-22 00:22:49 +0700

    test2

--- app/src/main/java/com/example/parking/MainActivity.kt
+++ app/src/main/java/com/example/parking/MainActivity.kt
@@ -8,14 +8,15 @@ import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
 
+    private val myParking: ArrayList<Slot> = ArrayList<Slot>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val slot1: Button = findViewById(R.id.slot1)
-        val slot2: Button = findViewById(R.id.slot2)
-        val slot3: Button = findViewById(R.id.slot3)
+        val Slot1: Button = findViewById(R.id.slot1)
+        val Slot2: Button = findViewById(R.id.slot2)
+        val Slot3: Button = findViewById(R.id.slot3)
 
         for(i in 0..2){
             myParking.add( i, Slot("", "", ""))
@@ -37,17 +38,17 @@ class MainActivity : AppCompatActivity() {
     fun click() {
         val slot1 = findViewById<Button>(R.id.slot1)
 
-        val plate = findViewById<EditText>(R.id.plate)
-        val name = findViewById<EditText>(R.id.name)
-        val brand = findViewById<EditText>(R.id.brand)
+        val aplate = findViewById<EditText>(R.id.plate)
+        val aname = findViewById<EditText>(R.id.name)
+        val abrand = findViewById<EditText>(R.id.brand)
 
-        var id = plate.text
-        var name = name.text
-        var brand = brand.text
+        var id = aplate.text
+        var name = aname.text
+        var brand = abrand.text
         findViewById<Button>(R.id.updatebtn).setOnClickListener {
             myParking.add( 0, Slot(id.toString(), brand.toString(), name.toString()))
-            slot1.setText(myParking.get(0).register).toString()
-            Log.d("register", myParking.get(0).register).toString()
+            slot1.setText(myParking.get(0).plate).toString()
+            Log.d("register", myParking.get(0).plate).toString()
         }
 
     }
@@ -55,16 +56,16 @@ class MainActivity : AppCompatActivity() {
     fun click2() {
         val slot2 = findViewById<Button>(R.id.slot2)
 
-        val plate  = findViewById<EditText>(R.id.plate)
-        val name = findViewById<EditText>(R.id.name)
-        val brand = findViewById<EditText>(R.id.brand)
+        val aplate = findViewById<EditText>(R.id.plate)
+        val aname = findViewById<EditText>(R.id.name)
+        val abrand = findViewById<EditText>(R.id.brand)
 
-        var id = plate.text
-        var name = name.text
-        var brand = brand.text
+        var id = aplate.text
+        var name = aname.text
+        var brand = abrand.text
         findViewById<Button>(R.id.updatebtn).setOnClickListener {
             myParking.add(1, Slot(id.toString(), brand.toString(), name.toString()))
-            slot2.setText(myParking.get(1).register).toString()
+            slot2.setText(myParking.get(1).plate).toString()
         }
 
     }
@@ -72,20 +73,20 @@ class MainActivity : AppCompatActivity() {
     fun click3() {
         val slot3 = findViewById<Button>(R.id.slot3)
 
-        val plate  = findViewById<EditText>(R.id.plate)
-        val name = findViewById<EditText>(R.id.name)
-        val brand = findViewById<EditText>(R.id.brand))
+        val aplate = findViewById<EditText>(R.id.plate)
+        val aname = findViewById<EditText>(R.id.name)
+        val abrand = findViewById<EditText>(R.id.brand)
 
-        var id = plate.text
-        var name = name.text
-        var brand = brand.text
+        var id = aplate.text
+        var name = aname.text
+        var brand = abrand.text
         findViewById<Button>(R.id.updatebtn).setOnClickListener {
             myParking.add(2, Slot(id.toString(), brand.toString(), name.toString()))
-            slot3.setText(myParking.get(2).register).toString()
+            slot3.setText(myParking.get(2).plate).toString()
         }
 
     }
 
 
 }
-}
+
